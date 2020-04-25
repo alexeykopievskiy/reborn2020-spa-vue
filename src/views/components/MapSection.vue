b5e83e30d90ea75bcd498ddae167177b2b9c6949
Alexey Kopievskiy <alexeykopievskiy@gmail.com>
2020-04-25 09:01:14 +0300

    add additional form

--- src/views/components/MapSection.vue
+++ src/views/components/MapSection.vue
@@ -4,7 +4,7 @@
       <div
         class="md-layout-item"
         style="height: 100%"
-        :class="(showGetHelp || showOfferHelp)?'md-size-60':'md-size-100'"
+        :class="(showGetHelp || showOfferHelp || showReborn)?'md-size-60':'md-size-100'"
       >
         <l-map
           ref="map"
@@ -123,6 +123,21 @@
               <md-input v-model="address" @keyup.enter.native="searchByAddress" />
             </md-field>
             <md-field>
+              <label for="business">Your type of business</label>
+              <md-select id="business" v-model="business_help" name="business">
+                <md-option value="entrepreneur">Entrepreneur</md-option>
+                <md-option value="coach">Coach</md-option>
+                <md-option value="bank">Bank</md-option>
+                <md-option value="service">Service</md-option>
+                <md-option value="owner of a public building">Owner of a public building</md-option>
+                <md-option value="goverment">Goverment</md-option>
+              </md-select>
+            </md-field>
+            <md-field>
+              <label>Or type other type of business</label>
+              <md-input v-model="business_help_other" />
+            </md-field>
+            <md-field>
               <label for="business">Business</label>
               <md-select id="business" v-model="business" name="business">
                 <md-option value="shop">Shop</md-option>
@@ -159,17 +174,21 @@
               <md-input v-model="issues_other" />
             </md-field>
             <md-field>
-              <label>Number of jobs in company</label>
-              <md-input v-model="jobs" />
+              <label for="help">Type of help</label>
+              <md-select id="help" v-model="help" name="help" multiple>
+                <md-option value="mentor">Mentor</md-option>
+                <md-option value="to issue a credit">To issue a credit</md-option>
+                <md-option value="give new clients">Give new clients</md-option>
+                <md-option value="to become a client">To become a client</md-option>
+                <md-option value="to buy services">To buy services</md-option>
+                <md-option value="to hire discharged stuff">To hire discharged stuff</md-option>
+                <md-option value="government support">Government support</md-option>
+              </md-select>
             </md-field>
             <md-field>
-              <label>What happened with your business</label>
-              <md-textarea v-model="problem" />
-            </md-field>
-            <md-field>
-              <label>What help do you need</label>
-              <md-textarea v-model="help" />
-            </md-field>
+              <label>Or type other type of issues</label>
+              <md-input v-model="help_other" />
+            </md-field> 
             <md-field>
               <label>Your social networks</label>
               <md-textarea v-model="social" />
@@ -184,7 +203,40 @@
             </md-field>
           </md-card-content>
           <md-card-actions class="d-flex justify-content-between">
-            <md-button @click="showGetHelp = false; removeMarker()" class="md-danger">Cancel</md-button>
+            <md-button @click="showOfferHelp = false; removeMarker()" class="md-danger">Cancel</md-button>
+            <md-button class="md-warning">Send Message</md-button>
+          </md-card-actions>
+        </md-card>
+      </div>
+      <div v-show="showReborn" class="md-layout-item md-size-40 md-small-size-100 ml-auto">
+        <md-card class="md-card-contact">
+          <md-card-header class="md-card-header-primary text-center">
+            <h4 class="card-title">Your story</h4>
+          </md-card-header>
+          <md-card-content>
+            <md-field>
+              <label>Your name</label>
+              <md-input v-model="name" />
+            </md-field>
+            <md-field>
+              <label>Company name</label>
+              <md-input v-model="company_name" />
+            </md-field> 
+            <md-field>
+              <label>Your story</label>
+              <md-textarea v-model="story" />
+            </md-field>
+            <md-field>
+              <label>Your email</label>
+              <md-input v-model="email" />
+            </md-field>
+            <md-field>
+              <label>Your phone</label>
+              <md-input v-model="phone" />
+            </md-field>
+          </md-card-content>
+          <md-card-actions class="d-flex justify-content-between">
+            <md-button @click="showReborn = false; removeMarker()" class="md-danger">Cancel</md-button>
             <md-button class="md-warning">Send Message</md-button>
           </md-card-actions>
         </md-card>
@@ -205,14 +257,7 @@
         <md-button v-scroll-to="'#map'" @click="showOfferHelp = true; showGetHelp = false" class="md-info md-lg">Offer Help</md-button>
       </div>
       <div class="md-layout-item md-size-30">
-        <v-popover offset="4" placement="top">
-          <md-button class="md-success md-lg">Reborn</md-button>
-
-          <template slot="popover">
-            <h3 class="popover-header">Not working</h3>
-            <div class="popover-body">Sorry, will be implemented in future</div>
-          </template>
-        </v-popover>
+        <md-button class="md-success md-lg" @click="showReborn = true; showOfferHelp = false; showGetHelp = false">Reborn</md-button>
       </div>
     </div>
   </div>
@@ -260,7 +305,13 @@ export default {
       business_other: null,
       address: null,
       name: null,
-      current_coords: null
+      business_help_other: null,
+      business_help: null,
+      help_other: null,
+      current_coords: null,
+      showReborn: false,
+      company_name: null,
+      story: null
     };
   },
   methods: {
@@ -283,8 +334,8 @@ export default {
       }
     },
     removeMarker() {
-      this.current_coords = null;
       this.center = this.currentCenter;
+      this.current_coords = null;
       this.markers = this.markers.splice(-1, 1);
       this.$refs.map.mapObject.invalidateSize();
       this.address = null;
